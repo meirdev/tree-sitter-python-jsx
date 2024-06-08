8a586358936c48bf0a6f77e860308e5f284d69e6
Meir <maornet@gmail.com>
2024-06-08 21:45:06 +0300

    remove empty string from children

--- jsx/types.py
+++ jsx/types.py
@@ -1,6 +1,11 @@
+import copy
 import dataclasses
 import json
+import re
 from typing import Any
+from collections.abc import Iterable
+
+SPACES = re.compile(r"\s+")
 
 
 @dataclasses.dataclass
@@ -34,7 +39,23 @@ class JSX:
     spread_props: list[Code] = dataclasses.field(default_factory=list)
 
     def __repr__(self) -> str:
-        props = str(self.props)
+        props = copy.deepcopy(self.props)
+
+        children = props.get("children")
+
+        if children is not None:
+            if not isinstance(children, Iterable):
+                children = [children]
+
+            children_ = [
+                i
+                for i in children
+                if not (isinstance(i, String) and SPACES.fullmatch(i.text.decode()))
+            ]
+
+            props["children"] = children_[0] if len(children_) == 1 else children_
+
+        props = str(props)
 
         if len(self.spread_props) > 0:
             props = (
